a9ae17363f227c7051ef9fc56325f9287b33ae84
Nenad Sparic <sparic.nenad@gmail.com>
2012-10-30 16:07:40 +0100

    user profile

--- src/Project/ProjectBundle/Controller/HomepageController.php
+++ src/Project/ProjectBundle/Controller/HomepageController.php
@@ -6,10 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-    	if($name) {
-	        return $this->render('PBundle:Homepage:index.html.twig', array('name' => $name));
-    	} else return $this->render('PBundle:Homepage:index.html.twig');
+    	// if($name) {
+	    //     return $this->render('PBundle:Homepage:index.html.twig', array('name' => $name));
+    	// } else return $this->render('PBundle:Homepage:index.html.twig');
+
+    	$user = $this->container->get('fos_user.user_manager')->findUserByUsername('nenad');
+
+    	echo "<pre>";
+    	print_r($user);
+    	echo "</pre>";
+    	exit;
+
+    	return $this->render('PBundle:Homepage:index.html.twig');
     }
 }
